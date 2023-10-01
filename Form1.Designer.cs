8f6bef76d21100351c0e66e745320a156df5b9bd
Murashko Vladislav <vladislavuh2000@gmail.com>
2023-10-01 21:18:07 +0300

    Lad task was completed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
+            BChooseDate = new Button();
+            label1 = new Label();
+            LResultDate = new Label();
+            DTP = new DateTimePicker();
             SuspendLayout();
+            // 
+            // BChooseDate
+            // 
+            BChooseDate.Font = new Font("Segoe UI", 18F, FontStyle.Italic, GraphicsUnit.Point);
+            BChooseDate.Location = new Point(274, 56);
+            BChooseDate.Name = "BChooseDate";
+            BChooseDate.Size = new Size(190, 63);
+            BChooseDate.TabIndex = 0;
+            BChooseDate.Text = "Choose the date";
+            BChooseDate.UseVisualStyleBackColor = true;
+            BChooseDate.MouseClick += OnMouseClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(194, 173);
+            label1.Name = "label1";
+            label1.Size = new Size(178, 40);
+            label1.TabIndex = 1;
+            label1.Text = "Chosen date:";
+            // 
+            // LResultDate
+            // 
+            LResultDate.AutoSize = true;
+            LResultDate.Font = new Font("Segoe UI", 21.75F, FontStyle.Italic, GraphicsUnit.Point);
+            LResultDate.Location = new Point(368, 173);
+            LResultDate.Name = "LResultDate";
+            LResultDate.Size = new Size(0, 40);
+            LResultDate.TabIndex = 2;
+            // 
+            // DTP
+            // 
+            DTP.Location = new Point(274, 255);
+            DTP.Name = "DTP";
+            DTP.Size = new Size(190, 23);
+            DTP.TabIndex = 3;
+            DTP.ValueChanged += OnValueChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(DTP);
+            Controls.Add(LResultDate);
+            Controls.Add(label1);
+            Controls.Add(BChooseDate);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DateTimePicker dateTimePicker1;
+        private Button BChooseDate;
+        private Label label1;
+        private Label LResultDate;
+        private DateTimePicker DTP;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,35 @@
+
+
 namespace WinFormsHT_2
 {
+    using System.Runtime.InteropServices;
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void OnMouseClick(object sender, MouseEventArgs e)
+        {
+            DTP.Open();
+        }
+
+        private void OnValueChanged(object sender, EventArgs e)
+        {
+            LResultDate.Text = DTP.Text.ToString();
+        }
+    }
+    public static class Extensions
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        private const uint WM_SYSKEYDOWN = 0x104;
+
+        public static void Open(this DateTimePicker obj)
+        {
+            SendMessage(obj.Handle, WM_SYSKEYDOWN, (int)Keys.Down, 0);
         }
     }
 }
